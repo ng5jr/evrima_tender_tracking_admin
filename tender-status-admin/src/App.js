afcbe96f52135c63abd413121fb5d7ca0d2beeac
Geronimo Nahuel Giordano <nahuelgiordano@hotmail.com>
2025-05-09 13:57:28 +0200

    operator tv

--- tender-status-admin/src/App.js
+++ tender-status-admin/src/App.js
@@ -39,7 +39,7 @@ function App() {
           }
         />
         <Route
-          path="/operator/tv"
+          path="/tv"
           element={
             <ProtectedRoute>
               {" "}
